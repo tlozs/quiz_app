8f1c90152c0771f0850c5c9f562d79450e637f5c
Gaál Zsolt <gaal.zsolt.19f@szlgbp.hu>
2024-11-18 20:33:57 +0100

    enhance input handling: add directory existence check and creation; improve path management functions

--- source/input.c
+++ source/input.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
+#include <limits.h>
 #include "input.h"
 
-const char * const input_location = "input/";
+const char * const input_root = "input";
 const char * const allowed_extensions[] = {".txt", ".csv", ".tsv"};
 const int allowed_extensions_size = sizeof(allowed_extensions) / sizeof(allowed_extensions[0]);
 
@@ -12,28 +13,58 @@ void read_all_input(int argc, char *argv[]) {
     int i;
     char c;
 
-    if (argc == 1) {
-        printf("Warning: no input files specified in the command line\n");
-        printf("Would you like to read all files in the input folder? (y/n) ");
+    if (no_input_root())
+    {
+        printf("\033[1;31mError: input directory not found\033[0m\n");
+        printf("Would you like to create the input directory? (y/n) ");
         c = getchar();
+        while (getchar() != '\n');
+        
         if (c == 'y' || c == 'Y') {
-            try_read_input("");
+            create_input_root();
+            printf("Input directory created\n");
+            printf("\nPlace your input files in the input directory, then press any key to continue\n");
+            printf("(If there is no \033[1;35mANY\033[0m key on your keyboard, consult your local keyboard vendor) ");
+            while ((c = getchar()) != '\n');
         }
+    }
+
+    if (argc == 1) {
+        printf("\033[1;33mWarning: no input parameters specified in the command line\033[0m\n");
+        printf("Would you like to read all files in the input directory? (y/n) ");
+        c = getchar();
+        while (getchar() != '\n');
+        if (c == 'y' || c == 'Y')
+            try_read_input(path_join(input_root, NULL));
         else
-            printf("No input files read\n");
+            printf("\033[1;31mError: No input files read\033[0m\n");
     }
     else
         for (i = 1; i < argc; i++)
-            try_read_input(argv[i]);
+            try_read_input(path_join(input_root, argv[i]));
 
     return;
 }
 
-char *search_last_dot(char *path_w_filename) {
+int no_input_root() {
+    DIR *dir = opendir(input_root);
+    if (dir) {
+        closedir(dir);
+        return 0;
+    }
+    else return 1;
+}
+
+void create_input_root() {
+    mkdir(input_root);
+    return;
+}
+
+char *search_last_dot(const char *path_w_filename) {
     return strrchr(path_w_filename, '.');
 }
 
-int extension_allowed(char *last_dot) {
+int extension_allowed(const char *last_dot) {
     int i;
     for (i = 0; i < allowed_extensions_size; i++)
         if (strcmp(last_dot, allowed_extensions[i]) == 0)
@@ -41,50 +72,76 @@ int extension_allowed(char *last_dot) {
     return 0;
 }
 
-void try_read_input(char *input) {
-    if (!try_read_file(input))
-        if (!try_read_folder(input))
-            printf("Error: could not read \'%s\'\n", input);
+char *path_join(const char* const path, const char* const target) {
+    char *target_path;
+    if (target == NULL)
+        target_path = strdup(path);
+    else {
+        target_path = malloc(strlen(path) + strlen(target) + 3);
+        sprintf(target_path, "%s\\%s", path, target);
+    }
+    return target_path;
+}
+
+int inside_input_root(const char *path) {
+    /* The absolute resolved path to the target (without references and symlinks) */
+    char resolved_path[PATH_MAX];
+    /* The path to the current working directory */
+    char cwd[PATH_MAX];
+    /* The full path to the input location (cwd + input_root) */
+    char *full_input_location;
+    /* whether the path starts with the input location */
+    int result;
+
+    /* If the path is not resolved or the current working directory is not found, return 0 */
+    if (!(_fullpath(resolved_path, path, PATH_MAX) && getcwd(cwd, sizeof(cwd))))
+        return 0;
+
+    full_input_location = path_join(cwd, input_root);
+
+    result = strncmp(resolved_path, full_input_location, strlen(full_input_location)) == 0;
+
+    free(full_input_location);
+    return result;
+}
+
+void try_read_input(char *input_path) {
+    if (inside_input_root(input_path)) {
+        if (!try_read_file(input_path) && !try_read_folder(input_path))
+            printf("\033[1;31mError: could not read \'%s\'\033[0m\n", input_path);
+    }
+    else
+        printf("\033[1;31mError: input \'%s\' is not within the input directory\033[0m\n", input_path);
+
+    free(input_path);
     return;
 }
 
-int try_read_file(char *path_w_filename) {
-    char *dot = search_last_dot(path_w_filename);
+int try_read_file(const char *target_location) {
+    char *dot = search_last_dot(target_location);
     FILE *file;
-    if (dot && extension_allowed(dot) && (file = find_file(path_w_filename))) {
+    if (dot && extension_allowed(dot) && (file = find_file(target_location))) {
         read_file(file);
         return 1;
     }
     return 0;
 }
 
-int try_read_folder(char *path_w_foldername) {
-    DIR *folder;
-    if ((folder = find_folder(path_w_foldername))) {
-        read_folder(folder, path_w_foldername);
+int try_read_folder(const char *target_location) {
+    DIR *folder = find_folder(target_location);
+    if (folder) {
+        read_folder(folder, target_location);
         return 1;
     }
     return 0;
 }
 
-FILE *find_file(char *path_w_filename) {
-    FILE* file;
-    char *file_location = malloc(strlen(input_location) + strlen(path_w_filename) + 1);
-    strcpy(file_location, input_location);
-    strcat(file_location, path_w_filename);
-    file = fopen(file_location, "r");
-    free(file_location);
-    return file;
+FILE *find_file(const char *file_location) {
+    return fopen(file_location, "r");
 }
 
-DIR *find_folder(char *path_w_foldername) {
-    DIR *folder;
-    char *folder_location = malloc(strlen(input_location) + strlen(path_w_foldername) + 1);
-    strcpy(folder_location, input_location);
-    strcat(folder_location, path_w_foldername);
-    folder = opendir(folder_location);
-    free(folder_location);
-    return folder;
+DIR *find_folder(const char *folder_location) {
+    return opendir(folder_location);
 }
 
 void read_file(FILE *file) {
@@ -96,7 +153,7 @@ void read_file(FILE *file) {
     return;
 }
 
-void read_folder(DIR *folder, char *folder_path) {
+void read_folder(DIR *folder, const char *folder_path) {
     struct dirent *entry;
     char *folder_path_extended;
     char *fname;
@@ -109,19 +166,13 @@ void read_folder(DIR *folder, char *folder_path) {
         if (strcmp(fname, ".") == 0 || strcmp(fname, "..") == 0) continue;
         
         /* 
-        we search relative to the input location, 
-        so we need to add the folder name to the searched file, since we are in the folder
-        (instead of 'input/a.txt' we need to read 'input/<we_are_in_this_folder>/a.txt')
+        since we are in the folder, we need to add the folder name to the searched file/folder
+        (instead of 'input/fname.txt' we need to read 'input/<we_are_in_this_folder>/fname.txt')
         */
-        folder_path_extended = malloc(strlen(folder_path) + strlen(fname) + 2);
-        strcpy(folder_path_extended, folder_path);
-        strcat(folder_path_extended, "/");
-        strcat(folder_path_extended, fname);
+        folder_path_extended = path_join(folder_path, fname);
 
         /* try to read entry as a file, if it fails it should be a folder so we recursively search it further */
         try_read_input(folder_path_extended);
-        
-        free(folder_path_extended);
     }
     closedir(folder);
     return;
--- source/input.h
+++ source/input.h
@@ -3,13 +3,25 @@
 #include <stdio.h>
 
 /* Location of the input files and folders */
-extern const char * const input_location;
+extern const char * const input_root;
 
 /* Allowed file extensions */
 extern const char * const allowed_extensions[];
 
 /* Number of allowed file extensions */
 extern const int allowed_extensions_size;
+
+/**
+ * Checks if the input directory exists.
+ * 
+ * @return 1 if the input directory is not found, 0 otherwise
+ */
+int no_input_root();
+
+/**
+ * Creates the input directory.
+ */
+void create_input_root();
 
 /**
  * Reads all input files and folders specified in the command line arguments.
@@ -27,7 +39,7 @@ void read_all_input(int argc, char *argv[]);
  * @param path_w_filename Path to the file
  * @return Pointer to the last dot in the file path, or NULL if no dot is found
  */
-char *search_last_dot(char *path_w_filename);
+char *search_last_dot(const char *path_w_filename);
 
 /**
  * Checks if the file extension is allowed.
@@ -37,19 +49,45 @@ char *search_last_dot(char *path_w_filename);
  * @param last_dot Pointer to the last dot in the file path
  * @return 1 if the extension is allowed, 0 otherwise
  */
-int extension_allowed(char *last_dot);
+int extension_allowed(const char *last_dot);
+
+/**
+ * Joins two paths together.
+ * 
+ * If target is NULL, returns a copy of the first path.
+ * 
+ * @param path First path
+ * @param target Second path
+ * @return Pointer to the joined path, should be freed after use
+ */
+char *path_join(const char* const path, const char* const target);
+
+/**
+ * Checks if the given path is within the input directory.
+ * 
+ * Resolving takes care of ../ references and symlinks.
+ * 
+ * The path is considered within the input directory if it starts with the input directory path.
+ * 
+ * @param path Path to the file or folder
+ * @return 1 if the path is within the input directory, 0 otherwise
+ */
+int inside_input_root(const char *path);
 
 /**
  * Tries to read the input first as a file, or if no file with the given name is found, as a folder.
  * 
  * If neither a file nor a folder is found, prints an error message.
  * 
+ * The input is treated relative to the defined input_location constant, 
+ * and the input is considered valid if it is within the input directory.
+ * 
  * @param input Path to the input file or folder
  */
-void try_read_input(char *input);
+void try_read_input(char *input_path);
 
 /**
- * Tries to read the input as a file.
+ * Tries to find and read a file at the given location.
  * 
  * Applies the following checks:
  * 
@@ -59,38 +97,34 @@ void try_read_input(char *input);
  * 
  * - The file is found
  * 
- * @param path_w_filename Path to the file
+ * @param target_location Path to the file
  * @return 1 if the file is successfully read, 0 otherwise
  */
-int try_read_file(char *path_w_filename);
+int try_read_file(const char *target_location);
 
 /**
- * Tries to read the input as a folder.
+ * Tries to find and read a folder at the given location.
  * 
- * @param path_w_foldername Path to the folder
+ * @param target_location Path to the folder
  * @return 1 if the folder is successfully read, 0 otherwise
  */
-int try_read_folder(char *path_w_foldername);
+int try_read_folder(const char *target_location);
 
 /**
  * Finds and opens a file at the given location.
  * 
- * Location is relative to the specified input_location constant.
- * 
- * @param path_w_filename Path to the file
+ * @param file_location Path to the file
  * @return Pointer to the opened file, or NULL if the file is not found
  */
-FILE* find_file(char *path_w_filename);
+FILE* find_file(const char *file_location);
 
 /**
- * Finds and opens a folder in the input location.
+ * Finds and opens a folder at the given location.
  * 
- * Location is relative to the specified input_location constant.
- * 
- * @param path_w_foldername Path to the folder
+ * @param folder_location Path to the folder
  * @return Pointer to the opened folder, or NULL if the folder is not found
  */
-DIR *find_folder(char *path_w_foldername);
+DIR *find_folder(const char *folder_location);
 
 /**
  * Reads the contents of a file and prints it to the standard output.
@@ -105,4 +139,4 @@ void read_file(FILE *file);
  * @param folder Pointer to the folder to be read
  * @param folder_path Path to the folder
  */
-void read_folder(DIR *folder, char *folder_path);
+void read_folder(DIR *folder, const char *folder_path);
